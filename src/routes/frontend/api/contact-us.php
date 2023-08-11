4eab236482de296d18dbbbd8c58acc53272d5c9e
GarlicBuns19 <planterx26@gmail.com>
2023-08-11 10:31:39 +0200

    added: email success and error templates
    fixed: phone number validation where it could not handle the zero in front

--- src/routes/frontend/api/contact-us.php
+++ src/routes/frontend/api/contact-us.php
@@ -1,5 +1,7 @@
 <?php
 
+use function Tina4\renderTemplate;
+
 \Tina4\Post::add("/api/frontend/contact-us", function (\Tina4\Response $response, \Tina4\Request $request) {
     $requestParams = $request->params;
     $emailHelper = (new Email());
@@ -13,11 +15,11 @@
     if (!$emailHelper->sendContactEmail($userName, $userEmail, $userTel, $userCompany, $userMessage, 1, $_ENV["RECEIVING_EMAIL"])) {
         // Email didn't send
         // Display error message
-        return $response ("Email could not be send. Please try again", HTTP_OK, TEXT_HTML);
+        return $response (renderTemplate("/frontend/email/contact-us-error.twig"), HTTP_OK, TEXT_HTML);
     }
 
     $emailHelper->sendContactEmail($userName, $userEmail, $userTel, $userCompany, $userMessage, 0, $userEmail);
 
     // Email sent
-    return $response ("Email has been sent. You will get feedback ASAP.", HTTP_OK, TEXT_HTML);
+    return $response (renderTemplate("/frontend/email/contact-us-success.twig"), HTTP_OK, TEXT_HTML);
 });
