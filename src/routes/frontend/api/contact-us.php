5cb17bcabd3f3411abe2bcac0c37779e51892ef5
GarlicBuns19 <planterx26@gmail.com>
2023-08-13 14:13:43 +0200

    updated: contact-us.php POST route

--- src/routes/frontend/api/contact-us.php
+++ src/routes/frontend/api/contact-us.php
@@ -23,7 +23,10 @@ use function Tina4\renderTemplate;
         return $response (["message" => "<h3>Email could not be sent. Please try again.</h3>"], HTTP_OK, TEXT_HTML);
     }
 
-    $emailHelper->sendContactEmail($userName, $userEmail, $userTel, $userCompany, $userMessage, 0, $userEmail);
+    // Notify user about failed email attempt to their email address
+    if (!$emailHelper->sendContactEmail($userName, $userEmail, $userTel, $userCompany, $userMessage, 0, $userEmail)) {
+        return $response (["message" => "<h3>An email could not be sent to your email address due to a problem, but your contact information has been received on our side. Please be patient.</h3>"], HTTP_OK, TEXT_HTML);
+    }
 
     // Email sent
     return $response (["message" => "<h3>Email has been sent. We will get back to you are soon as we can. Enjoy the rest of your day.</h3>"], HTTP_OK, TEXT_HTML);
