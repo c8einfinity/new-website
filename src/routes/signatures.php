21d8d2a8b3c9fc711519fad6243d4dbbee0155b4
Kobus van Wyk <kobus.vanwyk.84@gmail.com>
2023-09-11 17:50:55 +0200

    Styled Signature Creator
    Styled Signature Creator

--- src/routes/signatures.php
+++ src/routes/signatures.php
@@ -7,16 +7,88 @@
 
 \Tina4\Post::add("signature/creator", function (\Tina4\Response $response, \Tina4\Request $request) {
 
-    $signature =
-    _div(["style" => "background: pink"],
-        _img(["src" => "https://codeinfinity.co.za/images/logo.png", "style" => "width: 100px; height: 100px;"]),
-        "This is a test {$request->params["name"]}",
-        _div(["style" =>  "background:  green"],"Title :".$request->params["title"]),
-        _div(["style" =>  "background:  blue"],"Telephone :".$request->params["mobile"]),
-        _div(["style" =>  "background:  blue"],"Email :".$request->params["email"]),
-        html_entity_decode($request->params["additional_html"])
+    $signature = _table(["width" => "690", "cellpadding" => "0", "cellspacing" => "0", "style" => "background: #FFFFFF; width: 690px;"],
+        _tr(
+            _td(
+                _table(["width" => "100%", "cellpadding" => "0", "cellspacing" => "0"],
+                    _tr(
+                        _p(["style" => "font-family: Arial, sans-serif; font-size: 22px; margin: 0; color: #1ba6c5;"],
+                            _strong($request->params["name"])
+                        ),
+                        _p(["style" => "font-family: Arial, sans-serif; font-size: 16px; margin: 0; color: #000000; margin-top: 6px; margin-bottom: 4px;"],
+                            $request->params["title"]
+                        )
+                    ),
+                    _tr(
+                        _td(
+                            _img(["src" => "https://codeinfinity.co.za/images/c8-border-image.png", "alt" => "Divider Image"])
+                        )
+                    )
+                )
+            )
+        ),
+        _tr(
+            _td(
+                _table(["width" => "30", "cellpadding" => "0", "cellspacing" => "0", "style" => "padding-bottom: 5px;"],
+                    _tr(
+                        _td(
+                            _img(["src" => "https://codeinfinity.co.za/images/c8-signature-email.png", "alt" => "Email Icon", "style" => "width: 26px;"])
+                        ),
+                        _td(["style" => "padding-left: 15px;"],
+                            _p(["style" => "font-family: Arial, sans-serif; font-size: 14px; margin: 0;"],
+                                _a(["style" => "text-decoration: none; color: #000000", "href" => "mailto:" . $request->params["email"]], $request->params["email"])
+                            )
+                        )
+                    )
+                )
+            )
+        ),
+        _tr(
+            _td(
+                _table(["width" => "30", "cellpadding" => "0", "cellspacing" => "0"],
+                    _tr(
+                        _td(
+                            _img(["src" => "https://codeinfinity.co.za/images/c8-signature-phone.png", "alt" => "Phone Icon", "style" => "width: 26px;"])
+                        ),
+                        _td(["style" => "padding-left: 15px;"],
+                            _p(["style" => "font-family: Arial, sans-serif; font-size: 14px; margin: 0;"],
+                                _a(["style" => "text-decoration: none; color: #000000", "href" => "tel:" . $request->params["mobile"]], $request->params["mobile"])
+                            )
+                        )
+                    )
+                )
+            )
+        ),
+        _tr(
+            _td(
+                _table(["width" => "30", "cellpadding" => "0", "cellspacing" => "0"],
+                    _tr(
+                        _td(
+                            _img(["src" => "https://codeinfinity.co.za/images/c8-email-signature-banner.png", "style" => "width: 690px;"])
+                        )
+                    )
+                )
+            )
+        ),
+        _tr(
+            _td(["style" => "font-family: Arial, sans-serif; font-size: 12px; color: #707070; padding-top: 10px; padding-bottom: 18px;"],
+                "The content of this email is confidential and intended for the recipient specified in message only. It is strictly forbidden to share any part of this message with any third party, without a written consent of the sender. If you received this message by mistake, please reply to this message and follow with its deletion, so that we can ensure such a mistake does not occur in the future."
+            )
+        )
     );
 
+    $signature = _div(["style" => "background: #FFFFFF"], $signature,
+        _table(["width" => "690", "cellpadding" => "0", "cellspacing" => "0"],
+            _tr(
+                _td(["style" => "border-top: 1px solid #d4d4d4; padding-top: 10px;"],
+                    html_entity_decode($request->params["additional_html"]
+                    )
+                )
+            )
+        )
+    );
 
     return $response (\Tina4\renderTemplate("signature-creator.twig", ["signature" => $signature]), HTTP_OK, TEXT_HTML);
 });
+
+
